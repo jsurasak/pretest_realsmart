ceaf4c61db877b0e2fd791035dea44ed4eb3dbce
surasak <surasak.p@merudy.com>
2022-12-03 23:15:14 +0700

    update code

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -218,9 +218,12 @@ export default {
     };
 
     const functionrandomColor = () => {
-      var randomColor = Math.floor(Math.random() * 16777215).toString(16);
-
-      return "#" + randomColor;
+      var letters = "0123456789ABCDEF";
+      var color = "#";
+      for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
     };
 
     return {
